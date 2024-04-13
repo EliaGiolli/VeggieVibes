d80877bd5010d79dc5331279c898ea6972a01291
EliaGiolli <146369060+EliaGiolli@users.noreply.github.com>
2024-04-13 21:38:26 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+
+import ChangeBackground from './pages/ChangeBackground.jsx'
+import Search4Recipies from './pages/Search4Recipies.jsx'
+import RecipiesCards from './pages/RecipiesCards.jsx'
+
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/ChangeBackground",
+    element: <ChangeBackground />,
+  },
+  {
+    path: "/Search4Recipies",
+    element: <Search4Recipies />,
+  },
+  {
+    path: "/recipiesCards",
+    element: <RecipiesCards />,
+  },
+  {
+    path: "*",
+    element: <div>Not Found</div>,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/RecipiesCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RecipiesCards() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RecipiesCards
